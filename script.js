afd4a0c7956cd80292badacd16c43c41aa533814
Carlo Santos <rcarlosantos89@gmail.com>
2023-09-20 14:17:08 -0500

    review on objects

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-console.log("hello world");
+// console.log("hello world");
 
-for (let i = 0; i < 50; i++) {
-  console.log("Let's make 6 figures");
+// for (let i = 0; i < 50; i++) {
+//   console.log("Let's make 6 figures");
+// }
+
+
+let favoriteMoviesAndAlbums = {
+  favoriteMovies: ["The Thing", "Alien", "Hereditary", "Matrix"]
 }
+
+console.log(favoriteMoviesAndAlbums.favoriteMovies[0]);
